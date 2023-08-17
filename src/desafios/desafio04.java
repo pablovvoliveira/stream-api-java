8ee184ae76b82d54b13f22d97b8c3184926bd930
pablovvoliveira <pablovvoliveira@gmail.com>
2023-08-17 16:02:03 -0300

    Solução desafio 04.

---
+++ src/desafios/desafio04.java
@@ -0,0 +1,17 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class desafio04 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Desafio 4 - Remova todos os valores ímpares
+        numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+
+    }
+
+}
