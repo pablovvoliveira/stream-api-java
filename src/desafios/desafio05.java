798953c20255d2cfc6cea05143a6af3ba24ba264
pablovvoliveira <pablovvoliveira@gmail.com>
2023-08-17 16:17:01 -0300

    Solução desafio 05.

---
+++ src/desafios/desafio05.java
@@ -0,0 +1,28 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class desafio05 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Desafio 5 - Calcule a média dos números maiores que 5
+        List<Integer> maioresQueCinco = numeros.stream()
+                .filter(n -> n > 5)
+                .toList();
+
+        double media = maioresQueCinco.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+
+
+        System.out.println("Números maiores que 5: " + maioresQueCinco);
+        System.out.println("Média dos números maiores que 5: " + media);
+
+    }
+
+}
