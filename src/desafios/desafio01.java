a54f9835696fc9a4e1135968a64cb19d61eef129
pablovvoliveira <pablovvoliveira@gmail.com>
2023-08-17 15:30:56 -0300

    Solução desafio 01.

---
+++ src/desafios/desafio01.java
@@ -0,0 +1,16 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class desafio01 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Desafio 1 - Mostre a lista na ordem numérica:
+        numeros.sort(Comparator.naturalOrder());
+
+        System.out.println(numeros);
+    }
+}
