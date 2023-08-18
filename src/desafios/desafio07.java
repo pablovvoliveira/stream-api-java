4508400f0066b3bb36eebee2b576e45c502d6f72
pablovvoliveira <pablovvoliveira@gmail.com>
2023-08-17 22:46:33 -0300

    Solução desafio 07.

---
+++ src/desafios/desafio07.java
@@ -0,0 +1,23 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class desafio07 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Desafio 7 - Encontrar o segundo número maior da lista
+        int segundoMaiorNumero = numeros.stream()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .get();
+
+        System.out.println(segundoMaiorNumero);
+
+    }
+
+}
