81767f8b86c226f1f3339d3ed3c305105976803a
pablovvoliveira <pablovvoliveira@gmail.com>
2023-08-19 11:44:34 -0300

    Solução desafio 18.

---
+++ src/desafios/Desafio18.java
@@ -0,0 +1,31 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Desafio18 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Desafio 18 - Filtrar os números primos da lista
+        var primos = numeros.stream()
+                .filter(isPrimo)
+                .toList();
+
+        System.out.println(primos);
+    }
+
+    public static Predicate<Integer> isPrimo = n -> {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+}
