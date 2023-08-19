cad8087c62afb06737f00bf5681ac76648a479cc
pablovvoliveira <pablovvoliveira@gmail.com>
2023-08-19 10:56:20 -0300

    Solução desafio 13.

---
+++ src/desafios/Desafio13.java
@@ -0,0 +1,19 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio13 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Desafio 13 - Filtrar os números que estão dentro de um intervalo
+        List<Integer> intervalo = numeros.stream()
+                .filter(n -> n > 5 && n < 10)
+                .toList();
+
+        System.out.println(intervalo);
+
+    }
+
+}
